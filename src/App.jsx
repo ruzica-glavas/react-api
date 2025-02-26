6777c4f2be027144711b91a3ae6797a298be8163
ruzica-glavas <ruzi99@hotmail.it>
2025-02-26 16:51:27 +0100

    add: bootstrap and table

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,44 @@ import axios from 'axios';
 function App() {
   
 //Creazione di un array per la crezione dei posts
-
 const [posts, setPosts] = useState([])
 
 
 //Creazione funzione per api di posts
-
 function fetchPosts() {
   axios
   .get (`http://localhost:3000/posts`)
   .then ((res) => setPosts (res.data) )
 }
 
-//Chiamata del componente
-
+//Chiamata del componente quando carico la pagina
 useEffect(fetchPosts,[])
 
 
   return (
     <>
-      <ul>
-        {posts.map((post)=>(
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <table className="table">
+  
+
+  <thead>
+    <tr>
+    {posts.map((post)=>(
+      <th scope='row' key= {post.id}>{post.title}</th>
+    ))}
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+    {posts.map((post)=>(
+      <td scope='row' key= {post.id}>{post.content}</td>
+    ))}
+    </tr>
+      
+  </tbody>   
+    
+  
+</table>
     </>
   )
 }
