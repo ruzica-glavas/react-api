134f4848656d8f9165e2dbb1630ddf570dd40c8b
ruzica-glavas <ruzi99@hotmail.it>
2025-02-26 15:51:14 +0100

    add: axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function App() {
   
+//Creazione di un array per la crezione dei posts
+
+const [posts, setPosts] = useState([])
+
+
+//Creazione funzione per api di posts
+
+function fetchPosts() {
+  axios
+  .get (`http://localhost:3000/posts`)
+  .then ((res) => setPosts (res.data) )
+}
+
+//Chiamata del componente
+
+useEffect(fetchPosts,[])
+
 
   return (
     <>
-      
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
